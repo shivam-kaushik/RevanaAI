8a38cc5da9186eb267e4edd8cfe5553222d3aeef
shivam-kaushik <56489671+shivam-kaushik@users.noreply.github.com>
2025-10-31 13:30:54 -0400

    Refactored code

--- tmp_test_semantic.py
+++
@@ -1,5 +0,0 @@
-from backend.utils.vector_store import PostgresVectorStore
-
-vs = PostgresVectorStore()
-res = vs.semantic_search_customers("Find customers similar to 'CUST010'.", limit=5, dataset_id='default')
-print(res)
